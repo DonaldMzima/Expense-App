ccf40b9962128f79206dab2df307816c3a085b1f
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2022-11-05 15:42:21 +0200

    adding/date-constructor

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import ExpenseItem from './components/expenseItem'
 
 const App = () => {
   const expenses = [
-    { title: 'Toilet Paper', amount: 25000 },
-    { title: 'New Tv', amount: 25000 },
-    { title: 'insurance', amount: 25000 },
-    { title: 'New Desk (wooden)', amount: 25000 },
+    { title: 'Toilet Paper', amount: 25000, date: new Date(2022, 11, 1) },
+    { title: 'New Tv', amount: 25000, date: new Date(2022, 11, 1) },
+    { title: 'insurance', amount: 25000, date: new Date(2022, 11, 1) },
+    { title: 'New Desk (wooden)', amount: 25000, date: new Date(2022, 11, 1) },
   ]
 
   return (
@@ -15,18 +15,22 @@ const App = () => {
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
+        date={new Date(expenses[0].date)}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
+        date={new Date(expenses[0].date)}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
+        date={expenses[2].date}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
+        date={expenses[3].date}
       ></ExpenseItem>
     </div>
   )
